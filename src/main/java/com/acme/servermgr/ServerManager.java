82653754ddaeb022dc79e5fe656aea8725ca3918
jacobsiegel123 <siegel.yaakov@gmail.com>
2021-12-04 21:39:36 -0500

    added methods to the ServerManager class to tell us what each decorator is suppose to eventually add (to the string description)

--- src/main/java/com/acme/servermgr/ServerManager.java
+++ src/main/java/com/acme/servermgr/ServerManager.java
@@ -24,4 +24,21 @@ public class ServerManager {
     {
         return true;
     }
+
+    static public String getAvailableProcessors(){
+        return ", and there are 4 processors available";
+    }
+    static public String getFreeJVMMemory(){
+        return ", and there are 127268272 bytes of JVM memory free";
+    }
+    static public String getTotalJVMMemory(){
+        return ", and there is a total of 159383552 bytes of JVM memory";
+    }
+    static public String getJreVersion(){
+        return ", and the JRE version is 15.0.2+7-27";
+    }
+    static public String getTempLocation(){
+        return System.getenv("TEMP");
+    }
+
 }
